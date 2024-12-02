b0112bbbbab89670ba8367dfbf3441a486cf4562
perwejalam03 <perwejalam03@gmail.com>
2024-12-02 15:01:47 +0530

    all setup configured

--- src/db/index.js
+++ src/db/index.js
@@ -2,19 +2,17 @@ import mongoose from "mongoose";
 
 import { DB_NAME } from "../constants.js";
 
-const  connectDB = async () => {
-    try {
-        console.log(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        
-        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-           console.log(`\n Connected to MongoDB: ${connectionInstance.connection.host}`);
-           
-    } catch (error) {
-        console.log("MongoDB connection failed:", error);
-        process.exit(1);
-        
-    }
-}
-;
-
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.LOCAL_SERVER}/${DB_NAME}`
+    );
+    console.log(
+      `\nConnected to MongoDB: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+};
 export default connectDB;
